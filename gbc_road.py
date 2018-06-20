2b5e6868ab15f1f289e42a0e13c7509668ee4bd9
vpahari <vpahari@wesleyan.edu>
2018-06-20 11:46:29 -0400

    road

---
+++ gbc_road.py
@@ -0,0 +1,18 @@
+import pickle
+import networkx as nx
+
+BGU = nx.read_gpickle("BelgradeDriveU.gpickle")
+BJU = nx.read_gpickle("BeijingU.gpickle")
+NYU = nx.read_gpickle("NYU.gpickle")
+
+GBC_BG = nx.read_gpickle("GBC_BG.gpickle")
+GBC_BJ = nx.read_gpickle("GBC_BJ.gpickle")
+GBC_NY = nx.read_gpickle("GBC_NY.gpickle")
+
+print(len(BGU))
+print(len(BJU))
+print(len(NYU))
+
+print(len(GBC_BG))
+print(len(GBC_BJ))
+print(len(GBC_NY))
