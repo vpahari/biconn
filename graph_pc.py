d0979362084c8c6165c161a62098c3b10cfac11d
vpahari <vpahari@wesleyan.edu>
2018-07-20 14:42:48 -0400

    fd

---
+++ graph_pc.py
@@ -0,0 +1,78 @@
+import networkx as nx
+import osmnx as ox
+import random
+import math
+from operator import itemgetter
+import csv
+from functools import reduce
+import matplotlib.pyplot as plt
+
+plt.switch_backend('agg')
+
+def getGraphs(G, fileToSave):
+	
+	N = G.number_of_nodes()
+
+	numToRemove = int(N * 0.01)
+
+	initial_remove = int(N * 0.3)
+
+	init_removal = random.sample(list(G.nodes()),initial_remove)
+
+	G.remove_nodes_from(init_removal)
+
+	for i in range(11):
+		print(i)
+
+		nodesList = list(G.nodes())
+
+		conn = list(nx.connected_component_subgraphs(G))
+		biconn = list(nx.biconnected_component_subgraphs(G))
+
+		GC = max(conn, key=len)
+		GBC = max(biconn, key=len)
+
+		GCnodes = list(GC.nodes())
+		GBCnodes = list(GBC.nodes())
+
+		try:
+			fig, ax = ox.plot_graph(G,fig_height=30, fig_width=30)
+		except:
+			break
+
+		for n in GCnodes:
+			dictInfo = G.node[n]
+			ax.scatter(dictInfo['x'], dictInfo['y'], c='green')
+
+		GCName = fileToSave + "_GCPlot_%d.png" % (i) 
+		GBCName = fileToSave + "_GBCPlot_%d.png" % (i) 
+
+		plt.savefig(GCName)
+		plt.clf()
+
+		try:
+			fig, ax = ox.plot_graph(G,fig_height=30, fig_width=30)
+		except:
+			break
+
+		for n in GBCnodes:
+			dictInfo = G.node[n]
+			ax.scatter(dictInfo['x'], dictInfo['y'], c='red')
+
+		plt.savefig(GBCName)
+		plt.clf()
+
+		nodesToRemove = random.sample(nodesList,numToRemove)
+
+		G.remove_nodes_from(nodesToRemove)
+
+
+G = nx.read_gpickle("NYU.gpickle")
+
+getGraphs(G,"NYU_New")
+
+
+
+
+
+
