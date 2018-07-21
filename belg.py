059a57c6ec03e45ef9baa91e44cdc9ff9a2e1a2b
vpahari <vpahari@wesleyan.edu>
2018-07-21 13:11:21 -0400

    f

---
+++ belg.py
@@ -0,0 +1,146 @@
+import networkx as nx
+import osmnx as ox
+import random
+import math
+from operator import itemgetter
+import csv
+from functools import reduce
+import matplotlib.pyplot as plt
+
+plt.switch_backend('agg')
+
+def indexToTake(graphList,index):
+    gList = list(map(lambda x : x[index], graphList))
+
+    gAvg = reduce(lambda x,y : x+y, gList)
+
+    gAvg = gAvg / len(gList)
+
+    gStdDevList = list(map(lambda x : ((x - gAvg)**2),gList))
+
+    gStdDev = math.sqrt((reduce(lambda x,y: x+y,gStdDevList)) / len(gList))
+
+    return (gAvg,gStdDev)
+
+
+def createOrder(G):
+    allNodes = list(G.nodes())
+    degreeDict = dict(G.degree(allNodes))
+    degreeDictItems = list(degreeDict.items())
+    random.shuffle(degreeDictItems)
+    degreeDictItemsSorted = sorted(degreeDictItems, key = itemgetter(1),reverse = True)
+    onlyNodes = list(map(lambda x:x[0],degreeDictItemsSorted))
+    return onlyNodes
+
+
+def degree_removal(G, numNodesToRemove, remove_order, counter):
+    startIndex = numNodesToRemove * counter
+    endIndex = startIndex + numNodesToRemove
+    if endIndex > len(remove_order):
+        nodesToRemove = remove_order[startIndex:]
+    else:
+        nodesToRemove = remove_order[startIndex:endIndex]
+    
+    G.remove_nodes_from(nodesToRemove)
+    
+
+NY = nx.read_gpickle("BelgradeDriveU.gpickle")
+#NY_GBC = nx.read_gpickle("GBC_NY.gpickle")
+
+lc = []
+
+N = NY.number_of_nodes()
+
+k = float(2*(NY.number_of_nodes()/NY.number_of_edges()))
+
+step_size = 0.01
+
+numSimsOfGraphs = 2
+
+SEED = 3231
+
+for net_rep in range(numSimsOfGraphs):
+    print(net_rep)
+
+    s = SEED * (net_rep + 1)
+
+    f=0
+    counter = 0
+
+    lcList = []
+
+    G = NY.copy()
+
+    random.seed(s)
+
+    order = createOrder(G)
+
+    while G.number_of_nodes() > 0:
+
+        #assert G.number_of_nodes() == (N - counter*int(step_size*N))
+
+        conn = list(nx.connected_component_subgraphs(G))
+
+        if len(conn) == 0:
+            GC = nx.empty_graph(0)
+            GCLen = 0
+        else:
+            GC = max(conn, key=len)
+            GCLen = len(GC)
+
+        
+        lcList.append(GCLen)
+
+        if G.number_of_nodes() < int(step_size*N):
+            break
+
+        degree_removal(G,int(step_size*N), order, counter)
+
+        counter += 1
+        f = f + step_size
+
+    lc.append(lcList)
+
+
+fractions = 0
+
+counter = 0
+
+finalList = []
+
+lenList = len(lc[0])
+
+while (counter < lenList):
+    (avgGC, stdGC) = indexToTake(lc, counter)
+
+    finalList.append((fractions,avgGC, stdGC))
+
+    fractions = fractions + step_size
+    counter = counter + 1
+
+
+output_file_name = "BelgDegree2_N_%d_k_%g.csv"%(N,k)
+
+fh = open(output_file_name, 'w')
+writer = csv.writer(fh)
+
+writer.writerow(["f","GC avg","GC std"])
+
+for (fractions,avgGC, stdGC) in finalList:
+    writer.writerow([fractions,avgGC, stdGC])
+
+
+
+fh.close()
+
+
+
+
+
+
+
+
+
+
+
+
