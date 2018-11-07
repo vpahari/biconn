a5d084020135b1d6fbc8efea79200fb678c77e33
vpahari <vpahari@wesleyan.edu>
2018-11-06 22:34:00 -0500

    fd

--- community.py
+++ community.py
@@ -1,5 +1,12 @@
 import networkx as nx
 import networkit as nk
+import random
+
+def connect(G,nTC1,nTC2):
+
+
+
+
 
 N = 1000
 
@@ -21,19 +28,40 @@ print(list(G1.nodes()))
 
 print(list(G2.nodes()))
 
-for i in range(1000,2000):
+for i in range(N,2*N):
 	G1.addNode()
 
 print(list(G1.nodes()))
 
 for i,j in list(G2.edges()):
-	G1.addEdge(1000 + i, 1000 + j)
+	G1.addEdge(N + i, N + j)
 
 print(list(G1.edges()))
 
 print(list(G2.edges()))
 
-#(1993, 1957), (1994, 1914), (1995, 1035), (1995, 1043), (1995, 1262), (1995, 1265), (1995, 1359), (1995, 1407), (1995, 1409), (1995, 1535), (1995, 1661), (1996, 1664), (1996, 1955), (1996, 1981), (1997, 1433), (1997, 1595), (1997, 1931), (1998, 1307), (1998, 1854), (1998, 1920), (1999, 1097), (1999, 1337), (1999, 1445), (1999, 1751)]
+r = 10 ** (-4)
+
+numNodesToConnect = int(r * N)
+
+nodesInGraph = list(G1.nodes())
+
+N1 = nodesInGraph[:N]
+
+N2 = nodesInGraph[N:]
+
+#nodes to connect
+nTC1 = random.sample(N1, numNodesToConnect)
+nTC2 = random.sample(N2, numNodesToConnect)
+
+M_out = int(N / 10)
+
+
+
+
+
+
+
 
 
 
