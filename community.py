349369f2f10df367a1ce6af543bbc33e6a6644b0
vpahari <vpahari@wesleyan.edu>
2019-02-06 23:11:22 -0500

    fd

--- community.py
+++ community.py
@@ -395,7 +395,7 @@ connect_random_nodes(Gnk_1,100)
 
 step_size = 0.01
 
-percentage_to_attack = 0.2
+percentage_to_attack = 0.15
 
 
 (gc_List1,gc1_List1,gc2_List1,GC_nodes_List1, sgc_List1) = percolation(Gnk_1, step_size, "ABA", percentage_to_attack)
