65090a1093c342e825babf3e9dd6c53eaf20f7f5
vpahari <vpahari@wesleyan.edu>
2019-02-06 22:47:47 -0500

    fd

--- community.py
+++ community.py
@@ -394,7 +394,7 @@ copy_graph(Gnk_1)
 
 step_size = 0.01
 
-percentage_to_attack = 0.05
+percentage_to_attack = 0.1
 
 
 (gc_List1,gc1_List1,gc2_List1,GC_nodes_List1, sgc_List1) = percolation(Gnk_1, step_size, "ABA", percentage_to_attack)
