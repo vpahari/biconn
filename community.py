93a91758efded04bea6c9a983ab24772675e74c4
vpahari <vpahari@wesleyan.edu>
2019-02-13 15:14:45 -0500

    fd

--- community.py
+++ community.py
@@ -663,9 +663,9 @@ k = 3
 
 #edgesToAdd = int(N * edgesPercentage)
 
-Gnx_1 = nx.erdos_renyi_graph(N, k/(N-1), seed = 4232567)
+Gnx_1 = nx.erdos_renyi_graph(N, k/(N-1), seed = 21452)
 
-Gnx_2 = nx.erdos_renyi_graph(N, k/(N-1), seed = 1421678)
+Gnx_2 = nx.erdos_renyi_graph(N, k/(N-1), seed = 414653)
 
 Gnk_1 = nk.nxadapter.nx2nk(Gnx_1)
 
