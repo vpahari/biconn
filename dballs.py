bc09226a4bcbff0e91e9d7a5382cbf101c9282f3
vpahari <vpahari@wesleyan.edu>
2019-03-24 15:10:55 -0400

    df

--- dballs.py
+++ dballs.py
@@ -6,9 +6,8 @@ import math
 from functools import reduce
 import csv
 from operator import itemgetter
-import matplotlib
-matplotlib.use('agg')
 import matplotlib.pyplot as plt
+plt.switch_backend('agg')
 import pickle
 
 
