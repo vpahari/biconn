8859d9d8b1d32170c7f2a0dcf927df73c78962e2
vpahari <vpahari@wesleyan.edu>
2019-10-16 20:25:05 -0400

    f

--- adaptive_check_mean_degree.py
+++ adaptive_check_mean_degree.py
@@ -1263,9 +1263,11 @@ def new_optimal_attack(G_copy,radius,mean_deg_threshold):
 		print("counter:" + str(counter))
 		print(G_i.vcount())
 
+		print(dBall)
+
 		G_i.delete_vertices(dBall)
 
-		print(dBall)
+
 
 		(GC,SGC,num_comp,avg_comp_size) = get_GC_SGC_number_of_components(G)
 
