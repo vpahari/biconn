283bb2d8779988e764c2cadde55ce7f724436fa9
vpahari <vpahari@wesleyan.edu>
2020-09-02 13:19:35 +0200

    g

--- drawing_nx_MOD_NA_alpha.py
+++ drawing_nx_MOD_NA_alpha.py
@@ -728,17 +728,17 @@ def create_graphs(G, main_node, nodes_list, init_filename, position, counter, ra
 
 	plt.text(x, y - delta * 2,"ball_size = " + str(ball_len))
 
-	plt.text(x, y - delta * 3,"degree_main = " + str(degree_mainNode))
+	#plt.text(x, y - delta * 3,"degree_main = " + str(degree_mainNode))
 
-	plt.text(x, y - delta * 4,"between_main = " + str(between_mainNode))
+	#plt.text(x, y - delta * 4,"between_main = " + str(between_mainNode))
 
-	plt.text(x, y - delta * 5,"coreness_main = " + str(coreness_mainNode))
+	#plt.text(x, y - delta * 5,"coreness_main = " + str(coreness_mainNode))
 
-	plt.text(x, y - delta * 6,"degree_removed = " + str(removed_degree_str))
+	#plt.text(x, y - delta * 6,"degree_removed = " + str(removed_degree_str))
 
-	plt.text(x, y - delta * 7,"between_removed = " + str(removed_bet_str))
+	#plt.text(x, y - delta * 7,"between_removed = " + str(removed_bet_str))
 
-	plt.text(x, y - delta * 8,"coreness_removed = " + str(removed_core_str))
+	#plt.text(x, y - delta * 8,"coreness_removed = " + str(removed_core_str))
 
 	plt.savefig(new_filename)
 
